cb3ce501b8cc4e748facd68d55acda1204572b7e
dnozaki <dnozaki88888888@gmail.com>
2023-10-10 09:24:59 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import List from './components/List';
+import Add from './components/Add';
+
+function App() {
+  return (
+    <div className="App">
+      <Add />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Add.tsx
@@ -0,0 +1,29 @@
+import React, { useRef, MouseEvent } from 'react';
+import { useAddDispatch } from '../store/store';
+import { addPerson } from '../store/features/personSlice';
+
+const Add = () => {
+  const name = useRef<string>('');
+  const dispatch = useAddDispatch();
+  const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    dispatch(addPerson({ name: name.current }));
+  };
+  return (
+    <form className="border rounded-md p-2 shadow-md m-2">
+      <label htmlFor="">Person Name:</label>
+      <input
+        className="border rounded-md p-2 mx-2"
+        onChange={(e) => (name.current = e.target.value)}
+      />
+      <button
+        onClick={handleSubmit}
+        className="bg-violet-500 text-white rounded-md px-4 py-2 cursor-pointer hover:bg-violet-600 active: bg-violet-700"
+      >
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default Add;
